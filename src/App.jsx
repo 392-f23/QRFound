2e3b2d00c156709446e3c5a7a06b5b13ac5b8ba5
ploenvora <p.voraprukpisut@gmail.com>
2023-10-25 23:50:03 -0500

    added form template

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import Header from './components/header/Header';
-import ActivatePage from './ActivatePage';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import ActivatePage from "./ActivatePage";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header></Header>
-      <ActivatePage></ActivatePage>
+    <div className="app-div">
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/activate" element={<ActivatePage></ActivatePage>} />
+        </Routes>
+      </Router>
     </div>
   );
 };
