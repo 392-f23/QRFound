4910a9245d2206011902f411f89e06d02e23d942
Harvey-Y <498267405@qq.com>
2023-11-08 18:33:33 -0600

    Add distance display

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -45,6 +45,17 @@ function HomePage({ user }) {
     setSelectedItem(item.id);
   };
 
+  const haversine_distance = (mk1, mk2) => {
+    var R = 3958.8; // Radius of the Earth in miles
+    var rlat1 = mk1[1] * (Math.PI/180); // Convert degrees to radians
+    var rlat2 = mk2[1] * (Math.PI/180); // Convert degrees to radians
+    var difflat = rlat2-rlat1; // Radian difference (latitudes)
+    var difflon = (mk2[0]-mk1[0]) * (Math.PI/180); // Radian difference (longitudes)
+
+    var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+    return d;
+  };
+
   return user ? (
     <div className="home-container">
       <MapContainer
@@ -90,7 +101,7 @@ function HomePage({ user }) {
                     className="pin-image"
                     src={`/pin-${pinNumber}.png`}
                   ></img>
-                  <p>{item.color} {item.brand} {item.itemName}</p>
+                  <p>{item.color} {item.brand} {item.itemName} <span style={{color:'skyblue'}}>{haversine_distance(currentLocation, item.location).toFixed(2)} miles</span> </p>
                 </div>
                 {selectedItem === item.id && (
                   <button
